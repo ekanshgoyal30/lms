2946b8f06bb577afa33e0bb01c55c40ff820a2e6
ekanshgoyal30 <ekanshgoyal3009@gmail.com>
2026-04-19 15:47:01 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,9 +1,10 @@
 from database import get_connection
 
-def create_quiz(course_id, title):
+def create_quiz(course_id, title, time_limit=0, pass_score=0):
     conn = get_connection()
     c = conn.cursor()
-    c.execute("INSERT INTO quizzes (course_id, title) VALUES (?, ?)", (course_id, title))
+    c.execute("INSERT INTO quizzes (course_id,title,time_limit,pass_score) VALUES (?,?,?,?)",
+              (course_id, title, time_limit, pass_score))
     quiz_id = c.lastrowid
     conn.commit()
     conn.close()
@@ -12,10 +13,9 @@ def create_quiz(course_id, title):
 def add_question(quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option):
     conn = get_connection()
     c = conn.cursor()
-    c.execute("""
-        INSERT INTO questions (quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, (quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option.upper()))
+    c.execute("""INSERT INTO questions (quiz_id,question_text,option_a,option_b,option_c,option_d,correct_option)
+                 VALUES (?,?,?,?,?,?,?)""",
+              (quiz_id, question_text, option_a, option_b, option_c, option_d, correct_option.upper()))
     conn.commit()
     conn.close()
 
@@ -38,7 +38,7 @@ def get_questions(quiz_id):
 def save_attempt(student_id, quiz_id, score, total):
     conn = get_connection()
     c = conn.cursor()
-    c.execute("INSERT INTO quiz_attempts (student_id, quiz_id, score, total) VALUES (?, ?, ?, ?)",
+    c.execute("INSERT INTO quiz_attempts (student_id,quiz_id,score,total) VALUES (?,?,?,?)",
               (student_id, quiz_id, score, total))
     conn.commit()
     conn.close()
@@ -46,11 +46,9 @@ def save_attempt(student_id, quiz_id, score, total):
 def get_attempts(student_id, quiz_id):
     conn = get_connection()
     c = conn.cursor()
-    c.execute("""
-        SELECT score, total, attempted_at FROM quiz_attempts
-        WHERE student_id=? AND quiz_id=?
-        ORDER BY attempted_at DESC
-    """, (student_id, quiz_id))
+    c.execute("""SELECT score,total,attempted_at FROM quiz_attempts
+                 WHERE student_id=? AND quiz_id=? ORDER BY attempted_at DESC""",
+              (student_id, quiz_id))
     rows = [dict(r) for r in c.fetchall()]
     conn.close()
     return rows
